7c00bfc8b2d17cde1f0a971e987be7d455b0fed3
amosborne <amosborne@users.noreply.github.com>
2022-03-02 22:52:44 -0800

    suggest placement

--- dorfperfekt/map.py
+++ dorfperfekt/map.py
@@ -1,5 +1,5 @@
-from collections import defaultdict
 from collections.abc import MutableMapping
+from math import sqrt
 
 from .tile import Terrain, Tile
 
@@ -8,8 +8,7 @@ OFFSETS = set(zip(range(6), [(1, 0), (0, 1), (-1, 1), (-1, 0), (0, -1), (1, -1)]
 
 class Map(MutableMapping):
     def __init__(self):
-        self.tiles = defaultdict(lambda: None)
-        self.tiles[0, 0] = (Tile.from_string("g"), 0)
+        self.tiles = {(0, 0): (Tile.from_string("g"), 0)}
 
     def __getitem__(self, key):
         return self.tiles[key]
@@ -26,22 +25,17 @@ class Map(MutableMapping):
     def __len__(self):
         return len(self.tiles)
 
-    def neighbors(self, pos):
-        return {
-            (ori, npos)
-            for ori, opos in OFFSETS
-            if self[(npos := (pos[0] + opos[0], pos[1] + opos[1]))] is not None
-        }
+    def neighbors(self, pos, existing=True):
+        npos = lambda opos: (pos[0] + opos[0], pos[1] + opos[1])
+        neighbors = {(ori, npos(opos)) for ori, opos in OFFSETS}
+        real_neighbors = {(ori, npos) for ori, npos in neighbors if npos in self}
+        open_neighbors = neighbors - real_neighbors
+        return real_neighbors if existing else open_neighbors
 
     def open_positions(self):
-        return {
-            pos
-            for cpos in closed_positions
-            for _, pos in self.neighbors(cpos)
-            if pos not in (closed_positions := self.tiles.keys())
-        }
+        return {pos for cpos in self for _, pos in self.neighbors(cpos, existing=False)}
 
-    def is_valid_position(self, tile, pos, ori):
+    def is_valid_placement(self, tile, pos, ori):
         for sori, npos in self.neighbors(pos):
             this_terrain = tile[sori - ori]
             this_is_lake = all([t is Terrain.WATER for t in tile])
@@ -70,7 +64,7 @@ class Map(MutableMapping):
             pos
             for pos in self.open_positions()
             for ori in range(6)
-            if self.is_valid_position(tile, pos, ori)
+            if self.is_valid_placement(tile, pos, ori)
         }
 
     def is_ruined_position(self, pos):
@@ -94,3 +88,41 @@ class Map(MutableMapping):
                 return True
 
         return False
+
+    def rate_placement(self, tile, pos, ori):
+        # A placement's rating is a tuple -- lower numbers are better.
+        #  1. Number of tiles newly ruined by the placement (includes self).
+        #  2. Number of new open positions for future placements.
+        #  3. Euclidean distance to the origin.
+        #  4. Horizontal position.
+        #  5. Vertical position.
+        #  6. Orientation.
+
+        pre_ruined = sum(map(self.is_ruined_position, self))
+        pre_open = len(self.open_positions())
+
+        self[pos] = (tile, ori)
+
+        post_ruined = sum(map(self.is_ruined_position, self))
+        post_open = len(self.open_positions())
+
+        del self[pos]
+
+        return (
+            post_ruined - pre_ruined,
+            post_open - pre_open,
+            sqrt(pos[0] ** 2 + pos[1] ** 2),
+            *pos,
+            ori,
+        )
+
+    def rate_position(self, tile, pos):
+        rates = [self.rate_placement(tile, pos, ori) for ori in range(6)]
+        return sorted(rates)[0]
+
+    def suggest_placement(self, tile):
+        rates = [self.rate_position(tile, pos) for pos in self.open_positions()]
+        best = sorted(rates)[0]
+        npos = tuple(best[3:5])
+        ori = best[5]
+        return npos, ori
--- tests/test_map.py
+++ tests/test_map.py
@@ -6,25 +6,26 @@ def test_new_map():
     map = Map()
     assert map[0, 0] == (Tile.from_string("g"), 0)
     assert len(map) == 1
+    assert len(map.open_positions()) == 6
 
 
 def test_water():
     map = Map()
     map[0, 0] = (Tile.from_string("ggggwg"), 1)
     lake = Tile.from_string("w")
-    assert map.is_valid_position(lake, pos=(0, 1), ori=0)
-    assert map.is_valid_position(lake, pos=(1, -1), ori=0)
+    assert map.is_valid_placement(lake, pos=(0, 1), ori=0)
+    assert map.is_valid_placement(lake, pos=(1, -1), ori=0)
 
     river = Tile.from_string("ggwwgg")
-    assert map.is_valid_position(river, pos=(0, 1), ori=0)
-    assert map.is_valid_position(river, pos=(1, -1), ori=0)
-    assert not map.is_valid_position(river, pos=(1, 0), ori=0)
-    assert not map.is_valid_position(river, pos=(0, 1), ori=1)
-    assert not map.is_valid_position(river, pos=(1, -1), ori=1)
+    assert map.is_valid_placement(river, pos=(0, 1), ori=0)
+    assert map.is_valid_placement(river, pos=(1, -1), ori=0)
+    assert not map.is_valid_placement(river, pos=(1, 0), ori=0)
+    assert not map.is_valid_placement(river, pos=(0, 1), ori=1)
+    assert not map.is_valid_placement(river, pos=(1, -1), ori=1)
 
     map[0, 0] = (Tile.from_string("s"), 0)
-    assert map.is_valid_position(river, pos=(0, 1), ori=0)
-    assert map.is_valid_position(river, pos=(0, 1), ori=1)
+    assert map.is_valid_placement(river, pos=(0, 1), ori=0)
+    assert map.is_valid_placement(river, pos=(0, 1), ori=1)
 
 
 def test_train():
@@ -32,15 +33,15 @@ def test_train():
     map[0, 0] = (Tile.from_string("ggggtg"), 1)
 
     train = Tile.from_string("ggttgg")
-    assert map.is_valid_position(train, pos=(0, 1), ori=0)
-    assert map.is_valid_position(train, pos=(1, -1), ori=0)
-    assert not map.is_valid_position(train, pos=(1, 0), ori=0)
-    assert not map.is_valid_position(train, pos=(0, 1), ori=1)
-    assert not map.is_valid_position(train, pos=(1, -1), ori=1)
+    assert map.is_valid_placement(train, pos=(0, 1), ori=0)
+    assert map.is_valid_placement(train, pos=(1, -1), ori=0)
+    assert not map.is_valid_placement(train, pos=(1, 0), ori=0)
+    assert not map.is_valid_placement(train, pos=(0, 1), ori=1)
+    assert not map.is_valid_placement(train, pos=(1, -1), ori=1)
 
     map[0, 0] = (Tile.from_string("s"), 0)
-    assert map.is_valid_position(train, pos=(0, 1), ori=0)
-    assert map.is_valid_position(train, pos=(0, 1), ori=1)
+    assert map.is_valid_placement(train, pos=(0, 1), ori=0)
+    assert map.is_valid_placement(train, pos=(0, 1), ori=1)
 
 
 def test_ruined():
@@ -52,3 +53,34 @@ def test_ruined():
     map[1, 0] = (Tile.from_string("r"), 0)
     assert map.is_ruined_position((0, 0))
     assert map.is_ruined_position((1, 0))
+
+    assert len(map.open_positions()) == 10
+
+
+def test_suggest_placement():
+    map = Map()
+    rate = map.rate_placement(Tile.from_string("r"), (1, 0), 0)
+    assert rate == (2, 2, 1, 1, 0, 0)
+
+    rate = map.rate_position(Tile.from_string("r"), (1, 0))
+    assert rate == (2, 2, 1, 1, 0, 0)
+
+    npos, ori = map.suggest_placement(Tile.from_string("r"))
+    assert npos == (-1, 0)
+    assert ori == 0
+
+    one_grass = Tile.from_string("ffgfff")
+    npos, ori = map.suggest_placement(one_grass)
+    assert npos == (-1, 0)
+    assert ori == 4
+    map[npos] = (one_grass, ori)
+
+    npos, ori = map.suggest_placement(one_grass)
+    assert npos == (0, -1)
+    assert ori == 5
+    map[npos] = (one_grass, ori)
+
+    ranch_forest = Tile.from_string("ffrrrr")
+    npos, ori = map.suggest_placement(ranch_forest)
+    assert npos == (-1, -1)
+    assert ori == 0
